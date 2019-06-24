d33b58790ffed34931af784f7a15edad3eddf112
Paula Aguilera <pab.dominik@gmail.com>
2019-06-24 01:32:15 -0400

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+login = LoginManager( app )
+
 from app import views
