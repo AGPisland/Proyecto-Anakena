df27a17e6ceedaebcd7fe203da911cdf57a5d354
Paula Aguilera <pab.dominik@gmail.com>
2019-06-23 20:07:50 -0400

    add routes to login and logout

--- app/views.py
+++ app/views.py
@@ -131,3 +131,38 @@ def lista_de_alumnos():
         del cursos
         print(data)
         return render_template("lista_alumnos.html", valor=data)
+      
+@app.route( "/login" , methods = [ 'GET' , 'POST' ] )
+def login():
+    
+    if current_user.is_authenticated :
+
+        return redirect( url_for('index' ) )
+
+    form = LoginForm() 
+
+    if form.validate_on_submit() :
+        user = User.query.filter_by( username = form.username.data ).first()
+
+        if user.rol == "director":
+            return redirect( url_for('director'))
+
+        if user.rol == "profesor":
+            return redirect( url_for('profesor'))
+
+        if user.rol == "secretaria":
+            return redirect( url_for('secretaria'))
+
+        if user is None or not user.check_password( form.password.data ) :
+            flash( "Usuario o contraseña invalido" )
+            return redirect( url_for('login') )
+
+        login_user( user, remember = form.remember_me.data )
+        return redirect( url_for("index") )
+
+    return render_template ( "login.html" , title = "Home Page", form = form )
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect( url_for( "login" ) )
