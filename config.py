be1ce54483d15252471eeada10a8acc6594329dd
Paula Aguilera <pab.dominik@gmail.com>
2019-06-24 01:35:57 -0400

    fix comments

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-'''
+
 import os
 basedir = os.path.abspath( os.path.dirname( __file__ ) )
 
@@ -6,4 +6,4 @@ class Config( object ):
     SECRET_KEY = os.environ.get( "SECRET_KEY" ) or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get( "DATABASE_URL" )
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
-'''
+
